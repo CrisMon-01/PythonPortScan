28146a6bbc76e59a274aaf230298a8f59c9294ec
crismon-01 <cristianmonti1995@gmail.com>
2020-12-16 19:31:41 +0100

    uso file per porte e refactoring

--- python_scan.py
+++ python_scan.py
@@ -1,6 +1,19 @@
 import socket # appartiene a standar lib
+import json # app a starìndart lib
 
 OPEN_PORTS = []
+PORTS_DATA_FILE = "./common_ports.json"
+
+def extract_json_data(filename):
+    with open(filename, "r") as file:
+        data = json.load(file)  
+    return data
+
+def get_ports_info():
+    data = extract_json_data(PORTS_DATA_FILE)
+    #metodo items di python su diz. rit. tuple <k,v>
+    ports_info = {int(k): v for (k, v) in data.items()}
+    return ports_info
 
 def get_host_ip_addr(targername):
     try:
@@ -20,17 +33,22 @@ def scan_port(ip, port):
     sock.close()   #chiudi sempre conn
 
 # p.to ingresso allo script a se stante  
-# sto eseguendo questo script python
+# sto eseguendo questo script python 
 if __name__ == "__main__": 
+    # extract_json_data(PORTS_DATA_FILE)
     print("[INFO] PORT SCAN ")
     target = input("[IN] INSET HOSTNAME: ")
     ip_addr = get_host_ip_addr(target)
-    while True:
+    ports_info = get_ports_info()
+    #ciclo sulle key
+    for port in ports_info.keys():
         try:
-            port = int(input("[IN] INSERT PORT TO CHECK: "))
+            print(f"[INFO] SCANNING: {ip_addr}:{port}")
             scan_port(ip_addr, port)
-            print(OPEN_PORTS)
         except KeyboardInterrupt:
             print("\n")
             print("[INFO] STOP BY INTERRUPT ")
-            exit
+            break
+    print("[INFO] OPEN PORTS: ")
+    for port in OPEN_PORTS:
+        print(str(port), ports_info[port])  #uso portsinfo e prendo val
